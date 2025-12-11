fd21c76e6b40fe117114372c472ca803e79c147d
hagai240700 <yhagai240700@gmail.com>
2025-12-11 10:10:14 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,19 @@
+import React from "react"; // ייבוא React
+import ReactDOM from "react-dom/client"; // ייבוא ReactDOM ליצירת root
+import App from "./App.tsx"; // ייבוא קומפוננטת השורש App
+import "./index.css"; // ייבוא עיצוב כללי
+import { BrowserRouter } from "react-router-dom"; // ייבוא BrowserRouter לראוטינג
+
+ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement // יצירת root מתוך האלמנט עם id="root"
+).render(
+  <React.StrictMode> 
+    <BrowserRouter> 
+      <App /> 
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
