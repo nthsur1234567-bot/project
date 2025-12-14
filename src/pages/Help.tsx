d9f4f89368de3ec087adae785c22b72848a4247c
hagai240700 <yhagai240700@gmail.com>
2025-12-14 11:31:09 +0200

    Create Help.tsx

---
+++ src/pages/Help.tsx
@@ -0,0 +1,12 @@
+import React from "react"; // comment: react import
+
+const Help: React.FC = () => { // comment: help page component
+  return ( // comment: render help page
+    <div> {/* comment: wrapper */}
+      <h1>Help</h1> {/* comment: page title */}
+      <p>כאן יהיה מסך עזרה והסבר שימוש.</p> {/* comment: placeholder */}
+    </div> // comment: end wrapper
+  ); // comment: end return
+}; // comment: end component
+
+export default Help; // comment: export component
