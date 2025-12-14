4ceb7c2032d96c7a7d76779bae41ea89320b8b30
hagai240700 <yhagai240700@gmail.com>
2025-12-14 11:30:44 +0200

    Create Forms.tsx

---
+++ src/pages/Forms.tsx
@@ -0,0 +1,12 @@
+import React from "react"; // comment: react import
+
+const Forms: React.FC = () => { // comment: forms page component
+  return ( // comment: render forms page
+    <div> {/* comment: wrapper */}
+      <h1>Forms</h1> {/* comment: page title */}
+      <p>כאן יהיה מסך טפסים (בשלב הבא נעשה אותו עם MUI).</p> {/* comment: placeholder */}
+    </div> // comment: end wrapper
+  ); // comment: end return
+}; // comment: end component
+
+export default Forms; // comment: export component
