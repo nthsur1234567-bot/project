9966bf5605b1a81fa21fffe4eb5e049712f98159
hagai240700 <yhagai240700@gmail.com>
2025-12-14 11:32:13 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
-function Header() {
-  return (
-    <header>
-      <h1>מערכת הקורסים</h1>
-      <nav>
-        <a href="#">דף הבית</a> | <a href="#">הקורסים שלי</a> | <a href="#">צור קשר</a>
-      </nav>
-    </header>
-  );
-}
+import React from "react"; // comment: react import
+import { Link } from "react-router-dom"; // comment: link for navigation
 
-export default Header;
+const Header: React.FC = () => { // comment: header component
+  return ( // comment: render header
+    <nav style={{ display: "flex", gap: "12px", padding: "12px", borderBottom: "1px solid #ccc" }}> {/* comment: simple nav */}
+      <Link to="/">Home</Link> {/* comment: home link */}
+      <Link to="/management">Management</Link> {/* comment: management link */}
+      <Link to="/forms">Forms</Link> {/* comment: forms link */}
+      <Link to="/help">Help</Link> {/* comment: help link */}
+    </nav> // comment: end nav
+  ); // comment: end return
+}; // comment: end component
+
+export default Header; // comment: export
