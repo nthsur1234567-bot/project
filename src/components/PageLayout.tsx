1f1f8097729fd4cc47d48e9a0c8cfdeaf92a91b8
yehonatan hagai <yhagai240700@gmail.com>
2025-12-18 10:03:20 +0200

    pagelayout

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,30 +1,27 @@
 import React from "react"; // comment: react import
 import Container from "@mui/material/Container"; // comment: page container
-import Typography from "@mui/material/Typography"; // comment: title text
-import Box from "@mui/material/Box"; // comment: spacing/layout
+import Typography from "@mui/material/Typography"; // comment: title
+import Box from "@mui/material/Box"; // comment: layout spacing
 
-type Props = { // comment: component props
-  title?: string; // comment: optional page title
+type Props = { // comment: props
+  title?: string; // comment: optional title
   children: React.ReactNode; // comment: page content
 };
 
 const PageLayout: React.FC<Props> = ({ title, children }) => { // comment: layout component
   return (
-    <Container maxWidth="lg"> {/* comment: centered container */}
-      {title && ( // comment: show title only if provided
-        <Box sx={{ mt: 4, mb: 3 }}> {/* comment: spacing */}
-          <Typography variant="h4">
-            {title}
-          </Typography>
+    <Container maxWidth="lg">
+      {title && (
+        <Box sx={{ mt: 4, mb: 3 }}>
+          <Typography variant="h4">{title}</Typography>
         </Box>
       )}
 
-      <Box sx={{ pb: 6 }}> {/* comment: page content spacing */}
+      <Box sx={{ pb: 6 }}>
         {children}
       </Box>
     </Container>
   );
 };
 
-export default PageLayout; // comment: export
-
+export default PageLayout;
