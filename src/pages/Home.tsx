c9516b6dbed1b795f9521cee2d775c22ae8e0113
yehonatan hagai <yhagai240700@gmail.com>
2025-12-18 10:05:56 +0200

    home update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from "react"; // comment: react import
+import PageLayout from "../components/PageLayout"; // comment: layout
 
-const Home: React.FC = () => {
-  return (
-    <main style={{ padding: "20px" }}>
-      <h2>ברוך הבא לאפליקציית מנהל המשימות</h2>
-      <p>בחר עמוד מהתפריט כדי להתחיל לעבוד.</p>
-    </main>
-  );
-};
+const Home: React.FC = () => { // comment: home component
+  return ( // comment: render
+    <PageLayout title="Home"> {/* comment: layout wrapper */}
+      <p>ברוך הבא למערכת.</p> {/* comment: placeholder */}
+    </PageLayout>
+  ); // comment: end return
+}; // comment: end component
 
-export default Home;
+export default Home; // comment: export
