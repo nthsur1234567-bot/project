88dc77223db20ead4bd61a1fc2c70ece004fc375
yehonatan hagai <yhagai240700@gmail.com>
2025-12-18 10:01:51 +0200

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
-import React from "react"; // מייבא את React כדי להשתמש ב-JSX
-import ReactDOM from "react-dom/client"; // מייבא את ReactDOM ליצירת root בדפדפן
-import App from "./App.tsx"; // מייבא את קומפוננטת השורש App
-import "./index.css"; // מייבא את קובץ ה-CSS הראשי של האפליקציה
-import { BrowserRouter } from "react-router-dom"; // מייבא את BrowserRouter כדי לאפשר ראוטינג באפליקציה
+import React from "react"; // comment: react import
+import ReactDOM from "react-dom/client"; // comment: react dom root
+import { BrowserRouter } from "react-router-dom"; // comment: router wrapper
+import { ThemeProvider } from "@mui/material/styles"; // comment: theme provider
+import CssBaseline from "@mui/material/CssBaseline"; // comment: normalize css
+import App from "./App"; // comment: app component
+import theme from "./theme/theme"; // comment: custom theme
+import "./index.css"; // comment: global css
 
-ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement // יוצר root מתוך האלמנט עם id="root" בקובץ index.html
-).render(
-  <React.StrictMode>
-    {/* StrictMode עוזר לזהות בעיות פוטנציאליות בזמן פיתוח */}
-    <BrowserRouter>
-      {/* BrowserRouter עוטף את האפליקציה ומאפשר שימוש בנתיבים (Routes) */}
-      <App /> {/* מציג את קומפוננטת השורש של האפליקציה */}
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render( // comment: create root
+  <React.StrictMode> {/* comment: strict mode */}
+    <BrowserRouter> {/* comment: router wrapper */}
+      <ThemeProvider theme={theme}> {/* comment: provide MUI theme */}
+        <CssBaseline /> {/* comment: baseline styles */}
+        <App /> {/* comment: render app */}
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
+
