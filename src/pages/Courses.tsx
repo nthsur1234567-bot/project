90421c1fdb48786f18a8b008ff2f257f473836a2
hagai240700 <yhagai240700@gmail.com>
2025-12-18 08:46:34 +0200

    Create Courses.tsx

---
+++ src/pages/Courses.tsx
@@ -0,0 +1,7 @@
+import React from "react"; // comment: react import
+
+const Courses: React.FC = () => { // comment: page component
+  return <h1>ניהול קורסים</h1>; // comment: placeholder
+}; // comment: end component
+
+export default Courses; // comment: export
